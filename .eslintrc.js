22294210ce0c3292fc2d685f5b2c8c2f12b438c0
lizunlong <lizunlong@gmail.com>
2018-03-16 13:14:31 +0800

    Use ErrorOverlayPlugin

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['to', 'hrefLeft', 'hrefRight'],
+        aspects: ['noHref', 'invalidHref', 'preferButton'],
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: true,
+      },
+    ],
+  },
+}
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,11 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const history = require('connect-history-api-fallback')
 const convert = require('koa-connect')
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'development',
+  devtool: 'source-map',
   serve: {
     content: [__dirname],
     port: 3000,
@@ -13,4 +15,5 @@ module.exports = merge(common, {
       app.use(convert(history()))
     },
   },
+  plugins: [new ErrorOverlayPlugin()],
 })
