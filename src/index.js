8ca1e405854538043338ed26d763d0e1fc73f266
lizunlong <lizunlong@gmail.com>
2018-03-17 21:43:06 +0800

    Disable sw in dev

--- src/index.js
+++ src/index.js
@@ -2,28 +2,31 @@
 /* eslint no-console: ["error", { allow: ["log"] }] */
 import React from 'react'
 import { render } from 'react-dom'
-import OfflinePluginRuntime from 'offline-plugin/runtime'
 
 import App from './app'
 
 render(<App />, document.getElementById('app'))
 
-OfflinePluginRuntime.install({
-  onUpdating: () => {
-    console.log('SW Event:', 'onUpdating')
-  },
-  onUpdateReady: () => {
-    console.log('SW Event:', 'onUpdateReady')
-    // Tells to new SW to take control immediately
-    OfflinePluginRuntime.applyUpdate()
-  },
-  onUpdated: () => {
-    console.log('SW Event:', 'onUpdated')
-    // Reload the webpage to load into the new version
-    window.location.reload()
-  },
+if (process.env.NODE_ENV === 'production') {
+  // eslint-disable-next-line
+  const OfflinePluginRuntime = require('offline-plugin/runtime')
+  OfflinePluginRuntime.install({
+    onUpdating: () => {
+      console.log('SW Event:', 'onUpdating')
+    },
+    onUpdateReady: () => {
+      console.log('SW Event:', 'onUpdateReady')
+      // Tells to new SW to take control immediately
+      OfflinePluginRuntime.applyUpdate()
+    },
+    onUpdated: () => {
+      console.log('SW Event:', 'onUpdated')
+      // Reload the webpage to load into the new version
+      window.location.reload()
+    },
 
-  onUpdateFailed: () => {
-    console.log('SW Event:', 'onUpdateFailed')
-  },
-})
+    onUpdateFailed: () => {
+      console.log('SW Event:', 'onUpdateFailed')
+    },
+  })
+}
